455fa52e698d1f0fbe72e11c6c173b9284562dd4
Oussama Lachhab (Philo) <54874957+oussama-philo@users.noreply.github.com>
2024-12-06 10:50:33 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,41 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 int main()
 {
-	int fd1;
-	int rd;
-
-	char buff[8];
+	int fd1, fd2;
+	char *line1;
+	char *line2;
 
 	fd1 = open("txt2.txt", O_RDONLY);
+	fd2 = open("txt3.txt", O_RDONLY);
 
-	if (fd1 == -1)
+	if (fd1 == -1 || fd2 == -1)
 	{
 		perror("Error opening");
 		exit(EXIT_FAILURE);
 	}
 
-	while ((rd = read(fd1, buff, 8)) > 0)
-	{
-		buff[rd] = '\0';
-		printf("%s", buff);
-	}
+	line1 = get_next_line(fd1);
+	printf("line 1 : %s", line1);
+
+	line2 = get_next_line(fd2);
+	printf("line 1 : %s", line2);
+
+	line1 = get_next_line(fd1);
+	printf("line 2 : %s", line1);
+
+	line2 = get_next_line(fd2);
+	printf("line 2 : %s", line2);
+
+	line1 = get_next_line(fd1);
+	printf("line 3 : %s", line1);
+
+	line2 = get_next_line(fd2);
+	printf("line 3 : %s", line2);
+
+	free(line1);
+	free(line2);
+
 	close(fd1);
+	close(fd2);
 }
